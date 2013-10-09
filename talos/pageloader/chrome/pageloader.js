ac0d9afa3c71e1882150d5efa566c536a1c8b72d
Jordan Lund <jordan.l.lund@gmail.com>
2013-10-09 10:38:16 -0700

    reverts timeout in pageloader and popen args in ttest

--- talos/pageloader/chrome/pageloader.js
+++ talos/pageloader/chrome/pageloader.js
@@ -103,10 +103,10 @@ function plInit() {
 
     // In metro chrome runs, the browser window has our cmdline arguments. In
     // every other case they are on window.
-    setTimeout(function(){
-        dumpLine("JLUND waiting 5 seconds...")
-    }, 5000);
-    dumpLine("JLUND done waiting 5 seconds...")
+    // setTimeout(function(){
+    //     dumpLine("JLUND waiting 5 seconds...")
+    // }, 5000);
+    // dumpLine("JLUND done waiting 5 seconds...")
     let toplevelwin = Services.wm.getMostRecentWindow("navigator:browser");
     // toplevelwin_args_props = ''
     // toplevelwin_args_0 = ''
--- talos/ttest.py
+++ talos/ttest.py
@@ -354,12 +354,13 @@ class TTest(object):
 
                 b_cmd = self._ffprocess.GenerateBControllerCommandLine(command_line, browser_config, test_config)
                 try:
-                    process = subprocess.Popen(b_cmd,
-                                               universal_newlines=True,
-                                               bufsize=0,
-                                               env=os.environ,
-                                               stdout=subprocess.PIPE,
-                                               stderr=subprocess.STDOUT)
+                    process = subprocess.Popen(b_cmd, universal_newlines=True, bufsize=0, env=os.environ)
+                    # process = subprocess.Popen(b_cmd,
+                    #                            universal_newlines=True,
+                    #                            bufsize=0,
+                    #                            env=os.environ,
+                    #                            stdout=subprocess.PIPE,
+                    #                            stderr=subprocess.STDOUT)
                 except:
                     raise talosError("error executing browser command line '%s': %s" % (subprocess.list2cmdline(b_cmd), sys.exc_info()[0]))
 
@@ -395,9 +396,9 @@ class TTest(object):
                         val = cm.getCounterValue(count_type)
                         if val:
                             counter_results[count_type].append(val)
-                    line = process.stdout.readline()
-                    if line:
-                        utils.info(line)
+                    # line = process.stdout.readline()
+                    # if line:
+                    #     utils.info(line)
                     if process.poll() != None: #browser_controller completed, file now full
                         break
 
