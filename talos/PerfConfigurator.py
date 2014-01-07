b3a55819a2ef7b9e509cf4b3684c17234772d33d
Jordan Lund <jordan.l.lund@gmail.com>
2014-01-07 10:26:42 -0800

    removes global addon pref

--- talos/PerfConfigurator.py
+++ talos/PerfConfigurator.py
@@ -237,8 +237,6 @@ the highest value.
         'browser.shell.checkDefaultBrowser': False,
         'browser.warnOnQuit': False,
         'browser.display.overlaynavbuttons': False, # metrofx specific
-        # Bug 956384 - Enable addons for metro browser
-        'extensions.defaultProviders.enabled': True,  # metrofx specific
         'dom.allow_scripts_to_close_windows': True,
         'dom.disable_open_during_load': False,
         'dom.disable_window_flip': True,
